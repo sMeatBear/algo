0f9888d214f1f7c96c7ec886257839a9a9f378c7
Jeffrey Cao <fcandroidme@gmail.com>
2019-10-27 01:29:20 -0700

    create 430

--- lcode2/Leetcode2/src/app/linear/Flatten.java
+++ lcode2/Leetcode2/src/app/linear/Flatten.java
@@ -16,41 +16,24 @@ class Node {
         child = _child;
     }
 
+    public void init(int _val,Node _prev,Node _next,Node _child) {
+        val = _val;
+        prev = _prev;
+        next = _next;
+        child = _child;
+    }
+
     @Override
     public String toString() {
-        return val + "";
+        return val + "" + " pre: " + prev;
     }
 };
 
 class Solution {
-    public Node flatten2(Node h) {
-        return h;
-    }
-    
     public Node flatten(Node head) {
-        Node p = head;
-        while (p.next != null) {
-            if (p.child != null) {
-                Node[] child = traverse(p.child);
-                child[0].prev = p;
-                child[1].next = p.next;
-                p.next = child[0];
-                p.child = null;
-                p = child[1].next;
-            } else {
-                p = p.next;
-            }
-        }
-        
-        if (p.child != null) {
-                Node[] child = traverse(p.child);
-                child[0].prev = p;
-                child[1].next = p.next;
-                p.next = child[0];
-                p.child = null;
-                p = child[1].next;
-        }
-        return head;
+        if (head == null) return head;
+        Node[] res = traverse(head);
+        return res[0];
     }
     
     public Node[] traverse(Node a) {
@@ -65,7 +48,8 @@ class Solution {
                 child[1].next = a.next;
                 a.next = child[0];
                 a.child = null;
-                a = child[1].next;
+                // origial a next's prev should point to child[1]
+                child[1].next.prev = child[1];
             } else {
                 a = a.next;
             }
@@ -78,11 +62,33 @@ class Solution {
             child[1].next = a.next;
             a.next = child[0];
             a.child = null;
-            a = child[1].next;
+            a = child[1];
         }
         
         res[1] = a;
         
         return res;
+    }
+}
+
+public class Flatten {
+    public static void main(String[] args) {
+        Node n1 = new Node(), n2 = new Node(), n3 = new Node(), n4 = new Node(), n5 = new Node(),
+            n6 = new Node(), n7 = new Node();
+        n1.init(1, null, n2, null);
+        n2.init(2, n1, n5, n3);
+        n3.init(3, null, n4, null);
+        n4.init(4, n3, null, null);
+        n5.init(5, n2, null, n6);
+        n6.init(6, null, n7, null);
+        n7.init(7, n6, null, null);
+
+        Solution s = new Solution();
+        Node h = s.flatten(n1);
+        // Node h = n1;
+        while (h != null) {
+            System.out.println(h);
+            h = h.next;
+        }
     }
 }
