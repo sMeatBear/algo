52a2844a374786c4b7e0155638c497134fac59d8
sMeatBear <fcandroidme@gmail.com>
2020-01-12 15:20:31 -0800

    update no 39

--- lcode2/Leetcode2/src/app/array/no39_combinationsum/CombinationSum.java
+++ lcode2/Leetcode2/src/app/array/no39_combinationsum/CombinationSum.java
@@ -38,6 +38,32 @@ import java.util.Set;
 
 */
 class Solution {
+    // approach 4: optimized dp
+    public List<List<Integer>> combinationSum4(int[] cands, int t) {
+        Arrays.sort(cands); // sort candidates to try them in asc order
+        List<List<List<Integer>>> dp = new ArrayList<>();
+        for (int i = 1; i <= t; i++) { // run through all targets from 1 to t
+            List<List<Integer>> newList = new ArrayList<>(); // combs for curr i
+            // run through all candidates <= i
+            for (int j = 0; j < cands.length && cands[j] <= i; j++) {
+                // special case when curr target is equal to curr candidate
+                if (i == cands[j]) newList.add(Arrays.asList(cands[j]));
+                // if current candidate is less than the target use prev results
+                else for (List<Integer> l : dp.get(i-cands[j]-1)) {
+                    // Deduplicate, insert by ascending order
+                    if (cands[j] <= l.get(0)) {
+                        List<Integer> cl = new ArrayList<>();
+                        cl.add(cands[j]); cl.addAll(l);
+                        newList.add(cl);
+                    }
+                }
+            }
+            dp.add(newList);
+        }
+        return dp.get(t-1);
+    }
+
+
     // approch 3: backtracking
     private List<List<Integer>> res = new ArrayList<>();
     
