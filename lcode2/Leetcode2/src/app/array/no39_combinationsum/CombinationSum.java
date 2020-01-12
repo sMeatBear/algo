048e23ac8e350b483ad5674d79f5760c82d47a53
sMeatBear <fcandroidme@gmail.com>
2020-01-12 13:33:30 -0800

    update no39

--- lcode2/Leetcode2/src/app/array/no39_combinationsum/CombinationSum.java
+++ lcode2/Leetcode2/src/app/array/no39_combinationsum/CombinationSum.java
@@ -38,8 +38,28 @@ import java.util.Set;
 
 */
 class Solution {
+    // approach 2 backtracking:
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(list, new ArrayList<>(), nums, target, 0);
+        return list;
+    }
+    
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
+        if(remain < 0) return;
+        else if(remain == 0) list.add(new ArrayList<>(tempList));
+        else{ 
+            for(int i = start; i < nums.length; i++){
+                tempList.add(nums[i]);
+                backtrack(list, tempList, nums, remain - nums[i], i); // not i + 1 because we can reuse same elements
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+
     // approach 1 dp:
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    public List<List<Integer>> combinationSum1(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<>();
         if (candidates.length == 0) return res;
 
