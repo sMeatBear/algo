e3b3bc625227bcdf5bcaf0136861df75bc1f4f0a
sMeatBear <fcandroidme@gmail.com>
2020-01-13 23:32:16 -0800

    create no169

--- lcode2/Leetcode2/src/app/array/no169_majorityelement/MajorityElement.java
+++ lcode2/Leetcode2/src/app/array/no169_majorityelement/MajorityElement.java
@@ -1,5 +1,8 @@
 package app.array.no169_majorityelement;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
 
@@ -17,7 +20,28 @@ Output: 2
 
 class Solution {
     public int majorityElement(int[] nums) {
-        
+        int len = nums.length;
+        // majority number's restriction
+        int maj = len / 2;
+        int res = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            int cnt = map.getOrDefault(num, 0) + 1;
+            if (cnt > maj) return num;
+            map.put(num, cnt);
+        }
+
+        /* no need to traverse the map
+        for (Map.Entry<Integer, Integer> pair : map.entrySet()) {
+            if (pair.getValue() > maj) {
+                res = pair.getKey();
+                break;
+            }
+        }
+        */
+
+        return res;
     }
 }
 
