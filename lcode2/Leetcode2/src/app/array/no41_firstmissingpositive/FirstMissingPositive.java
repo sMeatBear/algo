6c9109512c6e10f502a0200e95332e94ebf3104b
sMeatBear <fcandroidme@gmail.com>
2020-01-15 10:17:44 -0800

    update no41

--- lcode2/Leetcode2/src/app/array/no41_firstmissingpositive/FirstMissingPositive.java
+++ lcode2/Leetcode2/src/app/array/no41_firstmissingpositive/FirstMissingPositive.java
@@ -1,5 +1,6 @@
 package app.array.no41_firstmissingpositive;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -23,9 +24,32 @@ Note:
 Your algorithm should run in O(n) time and uses constant extra space.
  */
 class Solution {
-    // approach 2: 
-    // approach 1: set
+    // Approach 2: Greedy traverse the nums to place each num in the right place if it's possible
     public int firstMissingPositive(int[] nums) {
+        // Greedy
+        for (int i = 0; i < nums.length; i++) {
+            int index = nums[i] - 1;
+            if (index >= 0 && index < nums.length && nums[index] != nums[i]) {
+                int tmp = nums[index];
+                nums[index] = nums[i];
+                nums[i] = tmp;
+                // place the num on this index rightly or cannot find the right one 
+                // then move to next number
+                i--;
+            }
+        }
+
+        // return first misplace num's index + 1
+        int i = 0;
+        for (; i < nums.length; i++) {
+            int val = i + 1;
+            if (nums[i] != val) return val;
+        }
+
+        return i + 1;
+    }
+    // approach 1: set
+    public int firstMissingPositive1(int[] nums) {
         Set<Integer> set = new HashSet<>();
         for (int i = 0; i < nums.length; i++) {
            set.add(nums[i]); 
@@ -43,6 +67,9 @@ class Solution {
 
 public class FirstMissingPositive {
     public static void main(String[] args) {
-        
+        int[] nums = new int[] {3,4,-1,1};
+        Solution s = new Solution();
+        int res = s.firstMissingPositive(nums);
+        System.out.println(res);
     }
 }
