e5634899f8a585f492fada5a4cc28e01b9f7f5c1
sMeatBear <fcandroidme@gmail.com>
2020-01-16 10:50:18 -0800

    create no516

---
+++ lcode2/Leetcode2/src/app/string/no516_longestsubstring/LongestSubstr.java
@@ -0,0 +1,61 @@
+package app.string.no516_longestsubstring;
+
+/**
+Given a string s, find the longest palindromic subsequence's length in s. You may assume that the maximum length of s is 1000.
+
+Example 1:
+Input:
+
+"bbbab"
+Output:
+4
+One possible longest palindromic subsequence is "bbbb".
+Example 2:
+Input:
+
+"cbbd"
+Output:
+2
+One possible longest palindromic subsequence is "bb".
+ */
+class Solution {
+    // approach 1: dp
+    public int longestPalindromeSubseq(String s) {
+        int[][] dp = new int[s.length()][s.length()];
+        
+        for (int i = s.length() - 1; i >= 0; i--) {
+            dp[i][i] = 1;
+            for (int j = i+1; j < s.length(); j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i+1][j-1] + 2;
+                } else {
+                    dp[i][j] = Math.max(dp[i+1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[0][s.length()-1];
+    }
+
+    public boolean isPalindromic(String s) {
+        int l = 0, r = s.length() - 1;
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+}
+
+
+public class LongestSubstr {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String str = "a";
+        int res = s.longestPalindromeSubseq(str);
+        System.out.println(res);
+    }
+}
