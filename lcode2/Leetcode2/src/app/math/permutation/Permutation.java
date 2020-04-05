85ce1f261ea30f760fe515f75502c672cf9050f9
sMeatBear <fcandroidme@gmail.com>
2020-04-05 14:21:51 -0700

    update permutation

--- lcode2/Leetcode2/src/app/math/permutation/Permutation.java
+++ lcode2/Leetcode2/src/app/math/permutation/Permutation.java
@@ -9,27 +9,32 @@ public class Permutation {
         while ( d > 0 && nums[d] <= nums[d - 1]) {
             d--;
         }
-        d = d > 0 ? d - 1 : 0;
 
-        // 2. Swap with the smallest number larger than current nums[d] on the right
-        int s;
-        for (s = d + 1; s < len && nums[d] <= nums[s]; s++) {
-        }
-
-        // 3. Swap
-        // In case s == 0:
-        s = s > 0 ? s - 1 : 0;
-        int tmp = nums[d];
-        nums[d] = nums[s];
-        nums[s] = tmp;
-
-        // 4. Sort the rest behind the d
-        if (d + 1 < len)
+        // Get the first descending element index
+        d--;
+        if (d < 0) {
+            // If only one element in the array or no descending elem from behind
+            Arrays.sort(nums);
+        } else {
+            // 2. Swap with the smallest number *larger than* current nums[d] on the right
+            int s;
+            for (s = d + 1; s < len && nums[d] < nums[s]; s++) {
+            }
+    
+            s--;
+            // 3. Swap
+            int tmp = nums[d];
+            nums[d] = nums[s];
+            nums[s] = tmp;
+    
+            // 4. Sort the rest behind the d
+            // if (d + 1 < len) No need for this statement. d must smaller than the nums.length !!
             Arrays.sort(nums, d + 1, len);
+        }
     }
 
     public static void main(String[] args) {
-        int[] nums = new int[] {2, 1, 3};
+        int[] nums = new int[] {1,2};
         Permutation.next(nums);
         System.out.println(Arrays.toString(nums));
     }
