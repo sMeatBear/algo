8c7f61203f26045d6a9588de326db9a0f27b8ff0
sMeatBear <fcandroidme@gmail.com>
2020-04-17 01:50:23 -0700

    Create InterfaceDefault

---
+++ lcode2/Leetcode2/src/app/basic/interfaceimpl/InterfaceDefault.java
@@ -0,0 +1,30 @@
+package app.basic.interfaceimpl;
+
+import java.util.List;
+
+/**
+ * test two interface with same method but with different implementation
+ * 
+ */
+interface CanBark {
+    default void bark() {
+        System.out.println("bark");
+    }
+}
+
+interface CanMakeNoise {
+    public void makeNoise();
+    default void bark() {
+        System.out.println("wang");
+    }
+}
+
+public class InterfaceDefault implements CanBark, CanMakeNoise {
+
+    @Override
+    public void makeNoise() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
