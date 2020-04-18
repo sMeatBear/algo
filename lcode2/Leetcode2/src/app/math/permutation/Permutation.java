4efe66ebcfd2b0f0b83678aea39775fec8f81e60
sMeatBear <fcandroidme@gmail.com>
2020-04-18 15:54:09 -0700

    Update Permutation

--- lcode2/Leetcode2/src/app/math/permutation/Permutation.java
+++ lcode2/Leetcode2/src/app/math/permutation/Permutation.java
@@ -3,6 +3,39 @@ package app.math.permutation;
 import java.util.Arrays;
 
 public class Permutation {
+    /**
+     * find the next permutation
+     * @param nums the array which is needed to do permutation
+     */
+    public static boolean nextPermutation(int[] nums) {
+        int len = nums.length;
+        // 1. find the first descending
+        int firstDescIdx = len - 2;
+        for (; firstDescIdx >= 0 && nums[firstDescIdx] >= nums[firstDescIdx + 1]; firstDescIdx--) {}
+
+        if (firstDescIdx < 0) {
+            // sort from the start
+            Arrays.sort(nums);
+            // become the first state
+            return false;
+        }
+
+        // 2. find the smallest element on the right which is larger than fisrt decending element
+        int minIdx = firstDescIdx + 1;
+        for (; minIdx < len - 1 && nums[minIdx + 1] > nums[firstDescIdx]; minIdx++) {}
+
+        // 3. swap
+        int tmp = nums[minIdx];
+        nums[minIdx] = nums[firstDescIdx];
+        nums[firstDescIdx] = tmp;
+
+        // 4. sort
+        Arrays.sort(nums, firstDescIdx + 1, len);
+
+        return true;
+    }
+
+    @Deprecated
     public static void next(int[] nums) {
         // 1. Find the first descending element:
         int len = nums.length, d = len - 1;
@@ -34,8 +67,9 @@ public class Permutation {
     }
 
     public static void main(String[] args) {
-        int[] nums = new int[] {1,2};
-        Permutation.next(nums);
-        System.out.println(Arrays.toString(nums));
+        int[] nums = new int[] {1,2,3,4};
+        do {
+            System.out.println(Arrays.toString(nums));
+        } while(Permutation.nextPermutation(nums));
     }
 }
