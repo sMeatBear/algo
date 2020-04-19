0c8c98266ec2ac137591309ac38469fe39860e57
sMeatBear <fcandroidme@gmail.com>
2020-04-18 21:32:30 -0700

    Create Coordinate.java

--- lcode2/Leetcode2/src/app/basic/generics/Cooridinate.java
+++ lcode2/Leetcode2/src/app/basic/generics/Cooridinate.java
@@ -19,4 +19,9 @@ public class Cooridinate<K, V> implements Pair<K, V> {
         return this.value;
     }
 
+    public boolean compare(Pair<K, V> p1, Pair<K, V> p2) {
+        K k1 = p1.getKey();
+        K k2 = p2.getKey();
+        return k1.equals(k2);
+    }
 }
