99ac74e81ea1ba5ff871dfd9046a5db08c95bef2
sMeatBear <fcandroidme@gmail.com>
2020-04-19 20:58:56 -0700

    Create FileSeparator

---
+++ lcode2/Leetcode2/src/app/basic/FileSeparator.java
@@ -0,0 +1,11 @@
+package app.basic;
+
+import java.io.File;
+
+public class FileSeparator {
+    public static void main(String[] args) {
+        System.out.println(File.separator);
+        System.out.println(File.pathSeparator);
+        System.out.println(File.separatorChar);
+    }
+}
