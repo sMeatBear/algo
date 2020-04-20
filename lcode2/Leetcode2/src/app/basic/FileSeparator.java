83e5e714cfa2716a7847349c76ab065aeacc80c2
sMeatBear <fcandroidme@gmail.com>
2020-04-19 22:38:55 -0700

    Cretea FileSeparator.java

--- lcode2/Leetcode2/src/app/basic/FileSeparator.java
+++ lcode2/Leetcode2/src/app/basic/FileSeparator.java
@@ -1,11 +1,17 @@
 package app.basic;
 
 import java.io.File;
+import java.util.Calendar;
 
 public class FileSeparator {
     public static void main(String[] args) {
         System.out.println(File.separator);
         System.out.println(File.pathSeparator);
         System.out.println(File.separatorChar);
+        long time = 1587357987 * 1000;
+        System.out.println("time: " + time);
+        Calendar cal = Calendar.getInstance();
+        cal.setTimeInMillis(time);
+        System.out.println(cal.getTime());
     }
 }
