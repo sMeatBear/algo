ed2d6dbd19a8a541af1dea77fed9403dc5e56f78
sMeatBear <fcandroidme@gmail.com>
2020-05-01 20:26:55 -0700

    Create pow function to MyMath

--- lcode2/Leetcode2/src/app/math/MyMath.java
+++ lcode2/Leetcode2/src/app/math/MyMath.java
@@ -1,5 +1,8 @@
 package app.math;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MyMath {
     /**
      * Compute the greatest common divisor
@@ -9,8 +12,97 @@ public class MyMath {
         return y;
     }
 
+
+    // approach 3:
+    public double myPow(double x, int shittyN) { 
+        long n = shittyN;
+        if (x == 0 || x == 1) {
+            return x;
+        }
+        if (x == -1) {
+            return n % 2 == 0 ? 1 : -1;
+        }
+        if(n == 0) return 1;
+        if(n < 0) {
+            n = -n;
+            x = 1 / x;
+        }
+        double ans = 1;
+        while(n > 0){
+            if(n % 2 == 1) ans *= x;
+            x *= x;
+            n >>= 1;
+        }
+        return ans;
+    }
+    
+    // iterative
+    public static double powItr(double x, int shittyN) {
+        long n = shittyN;
+        if (n == 0) {
+            return 1;
+        }
+        // corner case:
+        if(x == 0 || x == 1) {
+            return x;
+        }
+        if (x == -1) {
+            return Math.abs(n) % 2 == 0 ? 1 : -1;
+        }
+
+        if (n < 0) {
+            x = 1.0 / x;
+            n = -n;
+        }
+        Map<Integer, Double> cache = new HashMap<>();
+        cache.put(1, x);
+        double res = x;
+        for (int pow = 1, tmpPow = pow; pow < n;) {
+            if ((long)pow + tmpPow <= n) {
+                // double the power
+                res *= x;
+                x = res;
+                pow += tmpPow;
+                tmpPow = pow;
+                cache.put(pow, res);
+            } else {
+                while ((long)pow + tmpPow > n) {
+                    tmpPow >>>= 1;
+                }
+                x = cache.get(tmpPow);
+                res *= x;
+                pow += tmpPow;
+            }
+            if (res == 0) {
+                return res;
+            }
+        }
+        
+        return res;
+    }
+    
+    public static double pow(double x, int n) {
+            // corner case
+            if (x == 0 || x == 1) {
+                return x;
+            }
+            if (x == -1) {
+                return n % 2 == 0 ? 1 : -1;
+            } 
+
+            if (n == 0) {
+                return 1;
+            }
+            if (n > 0) {
+                return pow(x, n - 1);
+            }
+            return 1.0 / x * pow(x, n + 1);
+    }
+
     public static void main(String[] args) {
-        int gcd = gcd(10, 124);
-        System.out.println(gcd);
+        // int gcd = gcd(10, 124);
+        // System.out.println(gcd);
+        double res = pow(2, -13);
+        System.out.println(res == Math.pow(2, -13));
     }
 }
