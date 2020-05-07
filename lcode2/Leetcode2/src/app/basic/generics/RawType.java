6fcfa1fc051e0eacd71efe2943cda5d68b4858cc
sMeatBear <fcandroidme@gmail.com>
2020-05-07 15:00:38 -0700

    Update RawTypeTest

--- lcode2/Leetcode2/src/app/basic/generics/RawType.java
+++ lcode2/Leetcode2/src/app/basic/generics/RawType.java
@@ -8,7 +8,12 @@ public class RawType {
         List<Integer> list = new ArrayList<>();
         List rawList = list;
         rawList.add("hello");
+        rawList.add(1);
         System.out.println(rawList);
         System.out.println(list);
+        // Cannot perform instanceof check against parameterized type List<Integer>. 
+        // Use the form List<?> instead 
+        // since further generic type information will be erased at runtimeJava(536871458)
+        // System.out.println(list instanceof List<Integer>);
     }
 }
