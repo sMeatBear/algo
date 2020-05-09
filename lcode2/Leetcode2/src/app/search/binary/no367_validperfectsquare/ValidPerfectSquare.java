49ea722a922a96ecdc60e1647b24e704cf6b6605
sMeatBear <fcandroidme@gmail.com>
2020-05-09 12:13:18 -0700

    Update no367

--- lcode2/Leetcode2/src/app/search/binary/no367_validperfectsquare/ValidPerfectSquare.java
+++ lcode2/Leetcode2/src/app/search/binary/no367_validperfectsquare/ValidPerfectSquare.java
@@ -1,4 +1,4 @@
-package app.search.binary.no3677_validperfectsquare;
+package app.search.binary.no367_validperfectsquare;
 
 /**
  * Given a positive integer num, write a function which returns True if num is a perfect square else False.
