9a256faae1b7327be53289e43e0fefa99d1be419
sMeatBear <fcandroidme@gmail.com>
2020-05-12 01:16:58 -0700

    Create enum test

---
+++ lcode2/Leetcode2/src/app/basic/enumtest/Planet.java
@@ -0,0 +1,21 @@
+package app.basic.enumtest;
+
+public enum Planet {
+    MARS, EARTH(1.414), JUPITER, MERCURY, NEPTUNE, VENUS, SATURN, URANUS;
+
+    private double mass;
+    private Planet() {}
+
+    private Planet(double mass) {
+        this.mass = mass;
+    }
+
+    public double getMass() {
+        return mass;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(EARTH.getMass());    
+    }
+}
+
