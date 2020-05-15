ced948199d3a817d0ba6e70a68be227f5daa43d0
sMeatBear <fcandroidme@gmail.com>
2020-05-14 23:07:36 -0700

    Update UniqueBST

--- lcode2/Leetcode2/src/app/tree/no95_uniquebst/UniqueBST.java
+++ lcode2/Leetcode2/src/app/tree/no95_uniquebst/UniqueBST.java
@@ -1,6 +1,8 @@
 package app.tree.no95_uniquebst;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 import app.tree.TreeNode;
@@ -33,7 +35,71 @@ Submissions
 461,495
  */
 class Solution {
+    // approach 2: dp
+    /**
+     * Base rule: only 1
+     * Induction: add a larger element you can only add to its root or right subtree
+     */
     public List<TreeNode> generateTrees(int n) {
+        if (n <= 0) {
+            return new ArrayList<>();
+        }
+        List<TreeNode> prevRes;
+        List<TreeNode> list = new ArrayList<>();
+        list.add(new TreeNode(1));
+        prevRes = list;
+
+        for (int i = 2; i <= n; i++) {
+            list = new ArrayList<>();
+            for (int j = 0; j < prevRes.size(); j++) {
+                TreeNode root = prevRes.get(j);
+                // 1. take new node as the root
+                TreeNode newRoot = new TreeNode(i);
+                newRoot.left = root;
+                list.add(newRoot);
+
+                // 2. find all the right node make it as current left children and add the new node as right child
+                TreeNode curr = root;
+                while (curr.right != null) {
+                    TreeNode newNode = new TreeNode(i);
+                    TreeNode oriRight = curr.right;
+                    // change oriRight as left child of newNode
+                    newNode.left = oriRight;
+                    curr.right = newNode;
+                    list.add(copyATree(root));
+
+                    // then restore the origin structure!
+                    curr.right = oriRight;
+                    curr = curr.right;
+                }
+
+                // 3. add the new node to the right most place
+                curr.right = new TreeNode(i);
+                list.add(copyATree(root));
+                curr.right = null;
+            }
+            // update previous
+            prevRes = list;
+        }
+
+        return list;
+    }
+
+    /**
+     * Recursively copy a tree
+     */
+    private TreeNode copyATree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode newRoot = new TreeNode(root.val);
+        newRoot.left = copyATree(root.left);
+        newRoot.right = copyATree(root.right);
+        return newRoot;
+    }
+
+    // approach 1: recursive
+    public List<TreeNode> generateTrees1(int n) {
         if (n < 1) {
             return new ArrayList<>();
         }
