b39b9043b8d94ca33e988403c7e4ce6d08ba95d1
sMeatBear <fcandroidme@gmail.com>
2020-05-20 09:20:57 -0700

    Update ThrowException

--- lcode2/Leetcode2/src/app/basic/ThrowException.java
+++ lcode2/Leetcode2/src/app/basic/ThrowException.java
@@ -1,15 +1,22 @@
 package app.basic;
 
+import java.io.FileNotFoundException;
+
 public class ThrowException {
     public static void main(String[] args) {
         ThrowException te = new ThrowException();
-        te.testThrow(true);
-    }   
-    
-    public void testThrow(boolean throwException) {
+        te.test(true);
+    }
+
+    public void testThrow(boolean throwException) throws NullPointerException {
         if (throwException) {
+            // this is unchecked exception. No need to handle
             throw new NullPointerException("I was deliberate to do this");
         }
-        System.out.println("executed");;
+        System.out.println("executed");
+    }
+
+    public void test(boolean throwException) {
+        testThrow(throwException);
     }
 }
