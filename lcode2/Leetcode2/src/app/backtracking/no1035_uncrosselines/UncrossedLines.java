2e3a9e6bf94e927c174a2756bfb3058423254171
sMeatBear <fcandroidme@gmail.com>
2020-05-25 11:06:21 -0700

    Update no1035

--- lcode2/Leetcode2/src/app/backtracking/no1035_uncrosselines/UncrossedLines.java
+++ lcode2/Leetcode2/src/app/backtracking/no1035_uncrosselines/UncrossedLines.java
@@ -44,8 +44,20 @@ Note:
 1 <= A[i], B[i] <= 2000
  */
 class Solution {
-    // approach 1: dfs backtrack time exceeded
+    // approach 2: same as Longest Common Sequence
     public int maxUncrossedLines(int[] A, int[] B) {
+        int m = A.length, n = B.length, dp[][] = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; ++i)
+            for (int j = 1; j <= n; ++j)
+                if (A[i - 1] == B[j - 1])
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+        return dp[m][n];
+    }
+
+    // approach 1: dfs backtrack time exceeded
+    public int maxUncrossedLines1(int[] A, int[] B) {
         // <value, index list>
         Map<Integer, List<Integer>> aDict = new HashMap<>();
         for (int i = 0; i < A.length; i++) {
