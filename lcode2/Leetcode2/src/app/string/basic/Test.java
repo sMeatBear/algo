b1796140d2c5b73fe2baf3b61de32c2114b13770
sMeatBear <fcandroidme@gmail.com>
2020-06-16 08:53:13 -0700

    Create string test

---
+++ lcode2/Leetcode2/src/app/string/basic/Test.java
@@ -0,0 +1,9 @@
+package app.string.basic;
+
+public class Test {
+  public static void main(String[] args) {
+    String test = "hello";
+    System.out.println(Integer.parseInt(test));
+    System.out.println(Integer.valueOf(test));
+  } 
+}
