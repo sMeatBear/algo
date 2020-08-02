a9043db4fa9aff02a3ad05c52c665ae9bfe5b917
sMeatBear <fcandroidme@gmail.com>
2020-08-02 20:38:20 +0800

    Update lowerBound and upperbound of generics

--- lcode2/Leetcode2/src/app/basic/generics/LowerBoundAndUpperBound.java
+++ lcode2/Leetcode2/src/app/basic/generics/LowerBoundAndUpperBound.java
@@ -12,11 +12,12 @@ public class LowerBoundAndUpperBound {
     public static void main(String[] args) {
         List<? super Apple> testList = new ArrayList<>();        
         testList.add(new Apple());
-        testList.add(new Fruit()); // All the ancestors class of Apple (included) may be replace the wildcard
+        // testList.add(new Fruit()); // All the ancestors class of Apple (included) may be replace the wildcard
                                    // if ? == Apple, we cannot add Fruit
         testList.add(new Fuji()); // Fuji can upcast to Apple or its ancestor
-        Apple a = testList.get(0); // cannot determine which type get will return
+        // Apple a = testList.get(0); // cannot determine which type get will return
                                    // it may be Apple and its ancestor (cannot be downcasting automatically)
                                    // can cast mannually: (Apple) testList.get(0)
+        Fruit f = (Fruit) testList.get(0);
     }
 }
