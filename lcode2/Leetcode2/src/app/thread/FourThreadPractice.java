62ccec8f36ff02bb10d4765970ae2be043355f00
sMeatBear <fcandroidme@gmail.com>
2020-08-05 10:42:50 +0800

    Update FourThreadtest

--- lcode2/Leetcode2/src/app/thread/FourThreadPractice.java
+++ lcode2/Leetcode2/src/app/thread/FourThreadPractice.java
@@ -2,8 +2,43 @@ package app.thread;
 
 public class FourThreadPractice {
     int j;
+    
+    private synchronized void increaseJ() {
+        System.out.println(Thread.currentThread().getName() + " Inc " + ++j);
+    }
+
+    private synchronized void decreaseJ() {
+        System.out.println(Thread.currentThread().getName() + " Dec " + --j);
+    }
+
+    class IncreaseThread implements Runnable {
+        @Override
+        public void run() {
+            while (j < 10) {
+                increaseJ();
+            }
+        }
+    }
+
+    class DecreaseThread implements Runnable {
+        @Override
+        public void run() {
+            while (j > 0) {
+                decreaseJ();
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
         FourThreadPractice f = new FourThreadPractice();
+        for (int i = 0; i < 2; i++) {
+            Thread t = new Thread(f.new IncreaseThread());
+            t.start();
+            t = new Thread(f.new DecreaseThread());
+            t.start();
+        }
+        /*
         // thread 1 and thread 2 j++
         // thread 3 and thread 4 j--
         Thread th1 = new Thread(() -> {
@@ -61,6 +96,6 @@ public class FourThreadPractice {
         th1.start();
         th2.start();
         th3.start();
-        th4.start();
+        th4.start(); */
     }
 }
