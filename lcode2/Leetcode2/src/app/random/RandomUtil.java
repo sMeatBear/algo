6690d264b3162d37bc30cc3dcf65fe929742be2f
sMeatBear <fcandroidme@gmail.com>
2020-08-17 23:30:18 +0800

    Update getRandomNumbers

--- lcode2/Leetcode2/src/app/random/RandomUtil.java
+++ lcode2/Leetcode2/src/app/random/RandomUtil.java
@@ -1,9 +1,37 @@
 package app.random;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Random;
+import java.util.Set;
 
 public class RandomUtil {
+    /**
+     * Get specific number of random number (pseudo random number).
+     * Not even distributed in multiple invoking.
+     * @param begin the begin number (inclusive)
+     * @param end   the end number (exclusive)
+     * @param num   the required random number 
+     * @return      the list of generated random number
+     * @throws IllegalArgumentException if the given range is smaller than required number
+     */
+    public static List<Integer> getRandomNumbers(int begin, int end, int num) {
+        Set<Integer> result = new HashSet<>();
+        Random rand = new Random();
+        int range = end - begin;
+        if (range < num) {
+            throw new IllegalArgumentException("The range is smaller than required random number");
+        }
+
+        while (result.size() < num) {
+            result.add(rand.nextInt(range) + begin); // the range is from [begin, end - 1]
+        }
+
+        return new ArrayList<>(result);
+    }
+
     /**
      * Use an algorithm like insertion sort to swap the elements randomly
      * @param <T> generic type
@@ -70,5 +98,6 @@ public class RandomUtil {
         int[] arr = new int[] {1, 2, 3, 4, 5, 6};
         shuffleArray(arr);
         System.out.println(Arrays.toString(arr));
+        System.out.println(getRandomNumbers(1, 4, 2));
     }
 }
