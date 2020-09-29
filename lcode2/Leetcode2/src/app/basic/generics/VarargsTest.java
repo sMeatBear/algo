7b16bc5ed696c3de023a83208350430ddee9f165
sMeatBear <fcandroidme@gmail.com>
2020-09-29 10:27:48 +0800

    Create VarargsTest

---
+++ lcode2/Leetcode2/src/app/basic/generics/VarargsTest.java
@@ -0,0 +1,10 @@
+package app.basic.generics;
+
+public class VarargsTest {
+    // public static void test(String... s, int... i) {
+
+    // }
+    public static void main(String[] args) {
+        
+    }
+}
