2175c507913e3870db3e9ebb2d90061fd125e5d5
sMeatBear <fcandroidme@gmail.com>
2020-10-10 19:01:55 +0800

    Update queryTest

--- lcode2/Leetcode2/src/app/basic/annotation/annoimpl/dao/QueryTest.java
+++ lcode2/Leetcode2/src/app/basic/annotation/annoimpl/dao/QueryTest.java
@@ -3,15 +3,20 @@ package app.basic.annotation.annoimpl.dao;
 import app.Timer;
 import app.basic.annotation.annoimpl.domain.User;
 
-public class Query {
+public class QueryTest {
     public static void main(String[] args) {
         User userTable = new User();
         userTable.setName("Jack");
         userTable.setAge(18);
         userTable.setEmail("asdf@ff.com, dda@qu.com, da@cc.com");
+
+        Dept dept = new Dept();
+        dept.setDeptId(10);
+        dept.setDeptName("CS Department");
         DataBaseAccessor dba = new DataBaseAccessor();
         String stmt = (String) Timer.execute(dba, "query", new Object[] {userTable}, 
                                     Object.class);
         System.out.println(stmt);
+        System.out.println(dba.query(dept));
     }
 }
